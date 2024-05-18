1d355ffe3feb8f81900d40f52d0359e519c29eeb
ttlartey <ttlartey@st.ug.edu.gh>
2024-05-18 11:55:04 +0000

    Adding Task 1

---
+++ arrayManipulation.js
@@ -0,0 +1,13 @@
+// Function to process the array of numbers
+function processArray(arr) {
+    return arr.map(num => {
+      if (num % 2 === 0) {
+        // Square even numbers
+        return num * num;
+      } else {
+        // Triple odd numbers
+        return num * 3;
+      }
+    });
+  }
+  
