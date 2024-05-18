6635c825a300e02f222ec491ffe2d607e8a117ea
ttlartey <ttlartey@st.ug.edu.gh>
2024-05-18 11:58:46 +0000

    Adding Task 3

---
+++ userInfo.js
@@ -0,0 +1,18 @@
+let userIdCounter = 1;
+
+// Function to create user profiles
+function createUserProfiles(originalNames, modifiedNames) {
+  const userProfiles = [];
+  for (let i = 0; i < originalNames.length; i++) {
+    userProfiles.push({
+      originalName: originalNames[i],
+      modifiedName: modifiedNames[i],
+      id: userIdCounter++
+    });
+  }
+  return userProfiles;
+}
+
+module.exports = {
+  createUserProfiles
+};
